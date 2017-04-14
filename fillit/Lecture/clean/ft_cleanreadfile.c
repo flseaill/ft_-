7e9b75283b270cfa3b4bbbe197bee5b6f7c087a7
Florian SEAILLES <flseaill@e2r9p7.42.fr>
2017-04-14 22:16:07 +0200

    recuperation des pieces valide

--- fillit/Lecture/clean/ft_cleanreadfile.c
+++ fillit/Lecture/clean/ft_cleanreadfile.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_readfile.c                                      :+:      :+:    :+:   */
+/*   ft_cleanreadfile.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flseaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/04 13:52:16 by flseaill          #+#    #+#             */
-/*   Updated: 2017/04/14 21:53:51 by flseaill         ###   ########.fr       */
+/*   Created: 2017/04/14 22:09:29 by flseaill          #+#    #+#             */
+/*   Updated: 2017/04/14 22:09:51 by flseaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-int		ft_filesize(char *str);
 
 int		ft_strlen(char *str)
 {
@@ -45,107 +43,56 @@ int		ft_readfile(char *str)
 {
 	int		fd;
 	int		i;
+	int		pcsnbr;
 	char	c;
 	char	*pcs;
 
 	i = 0;
+
+	// Compteur du nombre de pieces.
+	pcsnbr = 0;
+
 	// Ouverture du Fichier.
 	fd = open(str, O_RDONLY);
-
+	
 	// Verif de la validite du fd + Allocation du stockage de la piece.
 	if (fd == -1 || (!(pcs = malloc(sizeof(char) * 22))))
-		// Erreur lors de l'execution de la fonction.
+		
+		// Erreur lors de l'execution.
 		return (0);
-
-	// Enregistrement pour lst chainee
+	
+	// Lecture
 	while (read(fd, &c, 1) > 0)
 	{
 		if (i == 21)
 		{
 			pcs[i] = '\0';
-			//ft_savepcs(pcs);
-			//ft_putchar('\n');
-			ft_putstr("piece suivante\n");
+
+			// Enregistrement de la piece.
+			//ft_savepcs(pcs, ++pcsnbr);
 			ft_putstr(pcs);
-			//ft_putchar('\n');
-			
-			// Reset.
+			++pcsnbr;
+			// Reset pour piece suivante.
 			i = 0;
 		}
-		// Enregistrement de la piece dans pcs.
+
+		// Enregistrement de la piece.
 		pcs[i] = c;
 		i++;
 	}
 	pcs[i] = '\0';
-	ft_putstr("piece suivante\n");
+
+	// Enregistrement de la derniere piece.
+	//ft_savepcs(pcs, ++pcsnbr);
 	ft_putstr(pcs);
-	// Enregistrement des pieces unes a unes dans **tabpcs.
-	/*
-	 *
-	 *
-	 while (read(fd, &c, 1) > 0)
-	 {
-	 if (i == 20)
-	 {
-	 tabpcs[j][i] = '\0';
-	 j++;
-	 i = 0;
-	 }
-	 tabpcs[j][i] = c;
-	 i++;
-	 }
-	 *
-	 *
-	 */
+	++pcsnbr;
 
-	// Creation de la liste chainee.
-	/*
-	 *
-	 *
-	 lst = ft_lstnew(pcs, 21);
-	 while (read(fd, &c, 1) > 0)
-	 {
-	 if (i == 20)
-	 {
-	 ft_lstadd(lst, *new);
-	 i = 0;
-	 }
-	 pcs[i] = c;
-	 i++;
-	 }
-	 *
-	 *
-	 */
-
-	// Test verif.
-	//ft_putstr(pcs);
-
-	// Deplacement de la variable dans une liste cree a son intention.
-	//ft_lstadd(lst, *new);
+	printf("\n%d\n", pcsnbr);
 
 	// Fermeture du fichier. + free de pcs.
 	close(fd);
 	free(pcs);
-
+	
 	// La fonction s'est bien execute.
 	return (1);
-}
-
-int		main(int argc, char *argv[])
-{
-	// Il manque des arguments.
-	if (argc > 2)
-		ft_putstr("Too many arguments.\n");
-
-	// Il y a trop d'arguments.
-	else if (argc < 2)
-		ft_putstr("File name missing.\n");
-
-	// Si le nombre de parametres est valide.
-	else if (argc == 2)
-	{
-		ft_readfile(argv[1]);
-	}
-	// FIN
-	return (0);
 }
