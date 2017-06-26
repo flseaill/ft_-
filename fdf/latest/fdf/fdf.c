24b6d277c2fb59a6ea57dc0efa77668cf82effbd
flseaill <Florianseailles@gmail.com>
2017-06-26 21:14:46 +0200

    gnl + fdf going further

--- fdf/latest/fdf/fdf.c
+++ fdf/latest/fdf/fdf.c
@@ -6,19 +6,24 @@
 /*   By: flseaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/21 18:42:29 by flseaill          #+#    #+#             */
-/*   Updated: 2017/05/21 19:06:36 by flseaill         ###   ########.fr       */
+/*   Updated: 2017/06/26 21:13:39 by flseaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include "fdf.h"
 #include <stdio.h>
 
-int		ft_display_key(int keycode, void *mlx)
+// Affiche le keycode sur lequel on appuie
+int		my_key_funct(int keycode, void *param)
 {
-	printf("key event %d\n", keycode);
+	printf("key event = %d\n", keycode);
+	// ft_putstr("key event = ");
+	// ft_putnbr(keycode);
+	// ft_putchar('\n');
 	return (0);
 }
 
+// Dessine un carre de la taille et la couleur donne
 int		ft_draw_square(void *mlx, void *win, int x, int y, int color)
 {
 	y = 50;
@@ -43,21 +48,32 @@ int		main(void)
 	int		x;
 	int		y;
 	int		color;
-	int		*winsize;
+	int		width;
+	int		height;
 
-	name = fdf;
-	winsize[0] = 720;
-	winsize[1] = 360;
-	winsize[2] = 0;
-	x = 720;
-	y = 360;
-	color = 0x00FFFFFF;
+	// Nom de la fenetre
+	name = "fdf";
+	// Largeur
+	width = 2560;
+	// Hauteur
+	height = 1440;
+	x = 1440;
+	y = 720;
+	color = ORANGE;
+	// Init de mlx
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, win, winsize[0], winsize[1], name);
+	// Init de la fenetre
+	win = mlx_new_window(mlx, width, height, name);
+	// Verif des inits
 	if (!mlx || !win)
 		return (0);
 	else
+		// x et y non pris en compte, pourquoi?
 		ft_draw_square(mlx, win, x, y, color);
-	mlx_key_hook(win, my_key_funct);
+	// Se dessine par dessus le premier carre, pourquoi?
+	// ft_draw_square(mlx, win, 2000, 800, 0x00ABACFC);
+	// Recuperation de la touche enfonce
+	mlx_key_hook(win, my_key_funct, 0);
+	// Pour que le programme ne se ferme pas
 	mlx_loop(mlx);
 }
